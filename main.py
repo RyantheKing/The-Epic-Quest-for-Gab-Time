108b5fd32a0f0663a70022592c5db7cd3961d819
RyantheKing <48844450+RyantheKing@users.noreply.github.com>
2019-12-10 09:18:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,12 @@
 # Initializing pygame + some important variables 
 
 import pygame
-import math
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-
-class Player(pygame.sprite.Sprite):
-    """
-    This class represents the player
-    It derives from the "Sprite" class in Pygame
-    """
-    info = {
-        "y": 200,
-        "x": 200,
-        "velocity": 5,
-        "width": 10,
-        "height": 27
-    }
-
-    image = {
-        "up": None,
-        "down": None,
-        "left": None,
-        "right": None,
-    }
-
-    def __init__(self):
-        """ Constructor. Pass in the color of the block,
-        and its x and y position. """
-        # Call the parent class (Sprite) constructor
-        super().__init__()
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        Player.image["right"] = pygame.image.load("sprites/player_right.png").convert()
-        Player.image["right"].set_colorkey(BLACK)
-        Player.image["right"] = pygame.transform.scale(Player.image["right"], (scale*Player.info["width"], scale*Player.info["height"]))
-
-        Player.image["left"] = pygame.image.load("sprites/player_left.png").convert()
-        Player.image["left"].set_colorkey(BLACK)
-        Player.image["left"] = pygame.transform.scale(Player.image["left"], (scale*Player.info["width"], scale*Player.info["height"]))
-
-        self.rect = Player.image["right"].get_rect()
-        self.rect.y = 300
-        self.rect.x = 400
-        self.image = Player.image["right"]
+import events
+from display import *
+from sprites import *
+from colors import *
 
 pygame.init()
-
-display = {
-    "width": 800,
-    "height": 600,
-    "gwidth": 256,
-    "gheight": 192
-}
-scale = math.floor(display["width"]/display["gwidth"])
 
 # Launching the window, setting it to the dimensions of the `display` dictionary.
 
@@ -71,6 +20,10 @@ player = Player()
 player.image = Player.image["right"]
 all_sprites_list.add(player)
 
+sprite_names = {
+    "player": player,
+}
+
 # The Main game loop
 while True:
     pygame.time.delay(30)
@@ -82,25 +35,9 @@ while True:
 
     #all_sprites_list.update()
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        player.image = Player.image["left"]
-        player.rect.x -= Player.info["velocity"]
-
-    if keys[pygame.K_RIGHT]:
-        player.image = Player.image["right"]
-        player.rect.x += Player.info["velocity"]
-
-    if keys[pygame.K_DOWN]:
-        player.rect.y += Player.info["velocity"]
-        
-
-    if keys[pygame.K_UP]:
-        player.rect.y -= Player.info["velocity"]
+    events.checkKeys(sprite_names)
         
     all_sprites_list.draw(win)
-    
     pygame.display.update()
 
 pygame.quit()
